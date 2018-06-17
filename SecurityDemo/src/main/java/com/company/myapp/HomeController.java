09a35aa04740924cce9633d57f516e6dc565e157
Pradeep <er.spider29@gmail.com>
2018-06-17 17:32:25 +0530

    Adding TestController

--- SecurityDemo/src/main/java/com/company/myapp/HomeController.java
+++ SecurityDemo/src/main/java/com/company/myapp/HomeController.java
@@ -64,4 +64,18 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value = "/ghi", method = RequestMethod.GET)
+	public String home3(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
 }
